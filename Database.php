239c6b187dab0eed1360b1e8c168b93616ad18de
Skolnieks <skolnieks.pjvt@pjvt.local>
2025-01-07 14:12:11 +0200

    sigma 3

--- Database.php
+++ Database.php
@@ -9,10 +9,10 @@ class Database {
         $this->pdo = new PDO($dsn);
         $this->pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
     }
-    public function query() {
+    public function query($sql, $params) {
 
-        $statement = $this->pdo->prepare("SELECT * FROM posts");
-        $statement->execute(); 
+        $statement = $this->pdo->prepare($sql);
+        $statement->execute($params); 
       return $statement;
     }
 }
--- index.php
+++ index.php
@@ -6,13 +6,16 @@ $config = require ("config.php");
 
 
 $db = new Database($config["database"]);
-$posts = $db->query("SELECT * FROM posts")->fetchAll();
 
-if (isset($_GET["search_query"]) && $_GET["search_query"] != "" );{
-    echo "Atgriezt ierakstus!";
-    $posts = $db->query("SELECT * FROM posts WHERE content LIKE '%" . $_GET["search_query"] . "%';")->fetchAll();
+
+$select = "SELECT * FROM posts";
+$params = [];
+if (isset($_GET["search_query"]) && $_GET["search_query"] != "" ){
+    $search_query = "%" . $_GET ["search_query"] . "%";
+    $select .= " WHERE content LIKE :nosaukums";
+    $params = ["nosaukums" => $search_query];
 };
-
+$posts = $db->query($select, $params)->fetchAll();
 
 
 echo "<h1>Blogs</h1>";
@@ -20,6 +23,10 @@ echo "<form>";
 echo "<input name='search_query'/>";
 echo "<button>Meklet</button>";
 echo "</form";
+
+if (count($posts) == 0) {
+echo "Not Found";
+}
 
 echo "<ul>";
 foreach ($posts as $post) {
